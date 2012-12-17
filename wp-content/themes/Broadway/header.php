2201149ac65577846abeffb32d54b0f65aca0d07
Dan Humphries <dan.humphries89@gmail.com>
2012-12-17 10:16:50 +0000

    fixed problem with stylesheet breaking the page

--- wp-content/themes/Broadway/header.php
+++ wp-content/themes/Broadway/header.php
@@ -3,9 +3,8 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-
 <title> <?php wp_title('|', true, 'right'); bloginfo( 'name' ); ?> </title>
-<link href="<?php echo get_stylesheet_dicretory_uri(); ?>/assets/css/normalize.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/normalize.css" rel="stylesheet" type="text/css" />
 <link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/base.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
