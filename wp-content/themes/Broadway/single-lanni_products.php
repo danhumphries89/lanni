9edb37ba753e9f5f58971b852a096af00ead29ba
Dan Humphries <dan.humphries89@gmail.com>
2012-12-17 12:35:42 +0000

    Begun styling of the product pages

--- wp-content/themes/Broadway/single-lanni_products.php
+++ wp-content/themes/Broadway/single-lanni_products.php
@@ -2,27 +2,48 @@
 
 	<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php $options_meta = get_post_meta(get_the_ID(), 'options', true); ?>
-
-		<?php the_content(); ?>
-
-
 		<?php 
-			
-			//split the options by the comma
-			$optionsList = explode(",", $options_meta);
+			//get the categories
+			$categories = get_the_category( get_the_ID() );
 
-			//loop through the list and execute the shortcodes
-			foreach($optionsList as $options) : 
-
-				//explode to get the attributes
-				$option_explode = explode(":", $options);
-
-				//execute the shortcode
-				echo do_shortcode( "[" . trim($option_explode[0]) . " " . trim($option_explode[1]) . "]" );
-
-			endforeach;
+			//get custom post data
+			$options_meta = get_post_meta(get_the_ID(), 'options', true); 
+			$price = get_post_meta(get_the_ID(), 'price', true);
 		?>
+
+
+		<div id="product<?php the_ID(); ?>" class="product-details">
+
+			<header class="product-header">
+				<h2 class="product-title"><?php the_title(); ?></h2>
+
+				<div class="price-block">
+					<span class="price">&pound;<?php echo $price; ?></span>
+				</div>
+			</header>
+
+			<article class="product-content">
+				<?php the_content(); ?>
+			</article>
+
+			<section class="product-options">
+				<?php 
+					//split the options by the comma
+					$optionsList = explode(",", $options_meta);
+
+					//loop through the list and execute the shortcodes
+					foreach($optionsList as $options) : 
+
+						//explode to get the attributes
+						$option_explode = explode(":", $options);
+
+						//execute the shortcode
+						echo do_shortcode( "[" . trim($option_explode[0]) . " " . trim($option_explode[1])	 . "]" );
+
+					endforeach;
+				?>
+			</section>
+		</div>
 
 	<?php endwhile; ?>
 
