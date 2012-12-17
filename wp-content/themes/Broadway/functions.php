252845ca27a125636be1f46af5b65af417657198
Dan Humphries <dan.humphries89@gmail.com>
2012-12-17 10:36:35 +0000

    created shortcode & added to template
    shortcode for colour options

--- wp-content/themes/Broadway/functions.php
+++ wp-content/themes/Broadway/functions.php
@@ -37,4 +37,29 @@ function add_product_taxonomies() {
     register_taxonomy_for_object_type('post_tag', 'lanni_products');
 }
 
+/** Add Shortcodes to Post Pages for Options **/
+function colourShortcode( $atts ){
+
+	$output = "<div class='row options'>"
+			 ."<select name='colour' id='colourOption'>"
+				 ."<option value='Red'>Red</option>"
+				 ."<option value='Pink'>Pink</option>"
+				 ."<option value='Orange'>Orange</option>"
+				 ."<option value='Silver'>Silver</option>"
+				 ."<option value='Gold'>Gold</option>"
+				 ."<option value='Dark Blue'>Dark Blue</option>"
+				 ."<option value='Light Blue'>Light Blue</option>"
+				 ."<option value='Red'>Red</option>"
+				 ."<option value='Dark Mauve'>Dark Mauve</option>"
+				 ."<option value='Dark Green'>Dark Green</option>"
+				 ."<option value='Turquoise'>Turquoise</option>"
+				 ."<option value='Green'>Green</option>"
+				 ."<option value='Black'>Black</option>"
+			 ."</select>"
+			 ."</div>";
+
+	return $output;
+}
+add_shortcode( 'colourOptions', 'colourShortcode' );
+
 ?>
--- wp-content/themes/Broadway/single-lanni_products.php
+++ wp-content/themes/Broadway/single-lanni_products.php
@@ -1,7 +1,17 @@
 <?php get_header(); ?>
 
 	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php $options_meta = get_post_meta(get_the_ID(), 'options', false); ?>
+
 		<?php the_content(); ?>
+
+		<?php 
+			foreach($options_meta as $options) : 
+				echo do_shortcode( "[" . $options . "]" );
+			endforeach;
+		?>
+
 	<?php endwhile; ?>
 
 <?php get_footer(); ?>
