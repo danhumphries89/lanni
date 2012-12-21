cefe9ccc0b6d05af7d1808f0990600e919de16d8
Dan Humphries <dan.humphries89@gmail.com>
2012-12-21 16:28:22 +0000

    setup new design navigation bar at the bottom of the screen

--- wp-content/themes/Broadway/footer.php
+++ wp-content/themes/Broadway/footer.php
@@ -1,9 +1,17 @@
-	<div id="shopping-footer" class="<?php echo (is_front_page()) ? "home" : "not-home"; ?>">
-
-		<header class="footer-header">
-			<span></span>
-		</header>
-
+	<div id="design_navigation">
+		<span class="designs navigation-title">Designs</span>
+		<ul class="design-list">
+			<?php wp_list_categories( array(
+						'orderby' => 'ASC',
+						'use_desc_for_title' => 0,
+						'child_of' => 5,
+						'title_li' => '',
+						'show_count' => 0,
+						'hide_empty' => 0
+					)
+				); 
+			?>
+		</ul>
 	</div>
 </div>
 
